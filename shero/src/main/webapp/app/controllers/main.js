89df2d91a9dc08462d8db0b447183f1510851e80
Fabian Boehm <fabian.boehm1@gmx.de>
2015-10-12 21:56:04 +0200

    - added font-awesome - added first working frontend version with navbar - different pages of page are loaded with angular

--- shero/src/main/webapp/app/controllers/main.js
+++ shero/src/main/webapp/app/controllers/main.js
@@ -7,7 +7,7 @@
  * # MainCtrl
  * Controller of the yeomanApp
  */
-angular.module('yeomanApp')
+angular.module('SHeroApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
