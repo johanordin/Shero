9ac22b3a895d88a57ce88b878e33bc7e0581390e
Pascal Jung <pascal.jung@student.hpi.uni-potsdam.de>
2015-10-12 19:13:19 +0200

    add objectify annotations

--- shero/src/main/java/edu/upc/ase/domain/Item.java
+++ shero/src/main/java/edu/upc/ase/domain/Item.java
@@ -1,12 +1,29 @@
 package edu.upc.ase.domain;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+
+import edu.upc.ase.helper.OfyHelper;
+
 /**
+ * The @Entity tells Objectify about our entity.  We also register it in {@link OfyHelper}
+ * Our primary key @Id is set automatically by the Google Datastore for us.
  *
- */
+ * Objectify, unlike the AppEngine library requires that you specify the fields you
+ * want to index using @Index.  Only indexing the fields you need can lead to substantial gains in
+ * performance -- though if not indexing your data from the start will require indexing it later.
+ *
+ **/
+@Entity
 public class Item {
+	@Id public Long id;
 	private String name;
-	private String location;
+	@Index private String location;
 	private double price;
+	
+	public Item() {
+	}
 	
 	public Item(String name, String location, double price) {
 		this.name = name;
--- shero/src/main/java/edu/upc/ase/domain/User.java
+++ shero/src/main/java/edu/upc/ase/domain/User.java
@@ -1,9 +1,18 @@
 package edu.upc.ase.domain;
 
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+@Entity
 public class User {
+	@Id public Long id;
 	private String name;
 	private String mail;
 	
+	public User() {
+		
+	}
+			
 	public User(String name, String mail) {
 		this.name = name;
 		this.mail = mail;
