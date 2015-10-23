cd7a564d04d73e86b35852c4ad6e69082ae9dd25
Fabian Boehm <fabian.boehm92@gmail.com>
2015-10-23 21:37:04 +0200

    added special controller for register-view of login-popup

---
+++ shero/src/main/webapp/app/controllers/register.js
@@ -0,0 +1,11 @@
+ /**
+ * @ngdoc function
+ * @name SHeroApp.controller:RegisterCtrl
+ * @description
+ * # RegisterCtrl
+ * Controller of the SHeroApp which is used for form fields of register-tab in the login-modal.
+ */
+angular.module('SHeroApp')
+	.controller('RegisterCtrl', function($scope) {
+
+    });
