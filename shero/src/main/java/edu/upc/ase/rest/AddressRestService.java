0d091c4ee55a7a93f5512377347d624589f97ddf
Pascal Jung <pascal.jung@student.hpi.uni-potsdam.de>
2015-10-27 00:31:05 +0100

    clean up imports

--- shero/src/main/java/edu/upc/ase/rest/AddressRestService.java
+++ shero/src/main/java/edu/upc/ase/rest/AddressRestService.java
@@ -3,7 +3,6 @@ package edu.upc.ase.rest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,7 +14,6 @@ import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
 
 import edu.upc.ase.domain.Address;
-import edu.upc.ase.domain.User;
 
 @Path("/addresses")
 public class AddressRestService {
