91f1248faac941272b2ddd178c487916fab7ead0
Pascal Jung <pascal.jung@student.hpi.uni-potsdam.de>
2015-10-30 01:27:17 +0100

    added querying functionality. queries can now filter by exact name, city, and price ranges (from - to)

--- shero/src/main/java/edu/upc/ase/rest/ItemRestService.java
+++ shero/src/main/java/edu/upc/ase/rest/ItemRestService.java
@@ -1,6 +1,7 @@
 package edu.upc.ase.rest;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -10,24 +11,54 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 import com.google.gson.Gson;
 import com.googlecode.objectify.Key;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.cmd.Query;
 
 import edu.upc.ase.domain.Item;
 
 @Path("/items")
 public class ItemRestService {
 	private static final Gson GSON = new Gson();
-
+	private static final Logger logger = Logger.getLogger("ItemRestService");
+	
+	/**
+	 * see: https://github.com/objectify/objectify/wiki/Queries
+	 * 
+	 * Also:
+	 * Properties used in inequality filters must be sorted first,
+	 * otherwise the result may not be complete, as only the findings 
+	 * until the first non-matching value will be contained.
+	 * 
+	 */
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String getItems() {
-		List<Item> items = ObjectifyService.ofy().load().type(Item.class)
-				.list();
-		return GSON.toJson(items);
+	public String getItemByExactName(@QueryParam("name") String itemName, @QueryParam("city") String cityName, @QueryParam("from") String from, @QueryParam("to") String to) {
+		// if no additional filters are added, this retrieves all items
+		Query<Item> q = ObjectifyService.ofy().load().type(Item.class);
+		
+		// filters are only added if the query parameter is specified
+		if (from != null && to != null) {
+			q = q.filter("price >=" , Double.valueOf(from)).filter("price <=", Double.valueOf(to));
+		}
+		
+		if (itemName != null) {
+			q = q.filter("name" , itemName); 
+		}
+		
+		if (cityName != null) {
+			q = q.filter("address.city =", cityName);
+		}
+		
+		// execute query
+		List<Item> results = q.list();
+		logger.info("query: " + q);
+		
+		return GSON.toJson(results);
 	}
 	
 	@GET
