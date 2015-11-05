fdc56ed0e4c3c665742a04e3d8a6fc8f331b00aa
KatsiarynaK <kate278@tut.by>
2015-11-05 12:01:50 +0100

    Password verification works

--- shero/src/main/webapp/app/controllers/editUser.js
+++ shero/src/main/webapp/app/controllers/editUser.js
@@ -36,27 +36,26 @@ angular.module('SHeroApp')
 
 		// call init function when controller is loaded
         $scope.init();
-
+		
+		//update user info
 	    $scope.processGeneralForm = function() {
+			
 	    	console.log($scope.generalForm);
 	    	console.log("genId: " + $scope.generalForm.id);
-
 	    	var userId = SessionStorageService.getUserId();
-	    	// TODO: hash password?
 	    	$http({
 	    		method: 'PUT', // TODO: should be PUT request for update
 	    		url: '/rest/users/' + userId,
 	    		data: $scope.generalForm
 	    	}).then(function successCallback(response) {
-			    console.log("success: " + JSON.stringify(response.data));
+				console.log("success: " + JSON.stringify(response.data));
+				alert ("Information was changed!");
 			}, function errorCallback(response) {
 			    console.log("error: " + response);
 			});
 	    }; 
-
-	    // TODO: offer possibility to delete an existing address
-		// DID: added delete button and request to delete an address
-		/*
+			    
+			
 		$scope.deleteAddress() = function() {
 			var addressId = $scope.addressData.id;
 			
@@ -67,7 +66,7 @@ angular.module('SHeroApp')
 	    		url: '/rest/addresses/' + addressId,
 	    		data: $scope.addressData 
 	    	}).then(function successCallback(response) {
-	    		SessionStorageService.deleteUserAddress(addressId);  // TODO: implement back-end function deleteUserAddress()! ? mayme deleteUserAddress(id??);
+	    		SessionStorageService.deleteUserAddress(addressId);  
 			    console.log("success: " + JSON.stringify(response.data));
                 alert ("Address deleted!");
 			}, function errorCallback(response) {
@@ -79,7 +78,7 @@ angular.module('SHeroApp')
 			
 		}
 		
-		*/
+		
 		// adds new address 
 	    $scope.processNewAddress = function() {
 	    	var userId = SessionStorageService.getUserId();
@@ -101,7 +100,8 @@ angular.module('SHeroApp')
 			}
 			
 	    }; 
-
+		
+		// update address
 	    $scope.processAddressForm = function() {
 	    	var addressId = $scope.addressData.id;
 			
@@ -115,6 +115,8 @@ angular.module('SHeroApp')
 	    		SessionStorageService.updateUserAddressSpecific(response.data);
 			    console.log("success: " + JSON.stringify(response.data));
                 alert ("Address changed!");
+				
+				
 			}, function errorCallback(response) {
 			    console.log("error: " + response);
 			});
@@ -123,12 +125,9 @@ angular.module('SHeroApp')
 	    }; 
 
 	    $scope.processPasswordForm = function() {
-
-	    	// TODO: make sure password and confirmed password match
-			// DID: replaced hashedPassword, added if-else
-
-	    	if ($scope.passwordForm.password.localeCompare($scope.passwordForm.repppassword)) { 
-			//var hashedPassword = $scope.passwordForm.password;
+	    	
+	    if ($scope.passwordForm.password.localeCompare($scope.passwordForm.reppassword) == 0) {
+					
 			var hashedPassword = Sha256.hash($scope.passwordForm.password);
 	    	var userId = SessionStorageService.getUserId();
 	    	console.log("id: " + userId);
@@ -139,17 +138,22 @@ angular.module('SHeroApp')
 	    		data: '{"hashedPassword":"' + hashedPassword + '"}'
 	    	}).then(function successCallback(response) {
 			    console.log("success: " + response);
+				alert ("Password changed!");
 			}, function errorCallback(response) {
 			    console.log("error: " + response);
 			});
 		} else {
-			// alert that password and confirmed password dont match
 			alert ("Password and Confirm password don't match!");
 		}
 		
 		
 	    }; 
 
+
+
+		
+		
+		
 	    //react on change of the view in the form and set variable of specified path to true
 		$scope.$watch( function () { return $location.path(); }, function (path) {
 			if (path=="/EditUser/General") {
