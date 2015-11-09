e2d49e80b44730117123291536931e2104c32cba
Max Dyrhage <maxdyrhage@gmail.com>
2015-11-09 18:01:47 +0100

    Added sorting on Price and Name

--- shero/src/main/webapp/app/controllers/searchresults.js
+++ shero/src/main/webapp/app/controllers/searchresults.js
@@ -45,7 +45,11 @@
       $scope.fetchItems();
       $('#expanding').html('<i class="fa fa-arrow-circle-up fa-5x" style="color:white" ng-click="expand">');
     };
-
+     
+        $scope.sort = function(keyname){
+        $scope.sortKey = keyname;   //set the sortKey to the param passed
+        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+    }
 
   });
 //to show the rating as stars
@@ -59,5 +63,7 @@ angular.module('SHeroApp').filter('range', function() {
 
     return input;
   };
+    
+ 
 
 });
