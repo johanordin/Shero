4f46006537ea4eb0eec85bf7e5a5843c638d6678
Fabian Boehm <fabian.boehm92@gmail.com>
2015-11-10 13:19:28 +0100

    finished draft of search-result list

--- shero/src/main/webapp/app/controllers/searchresults.js
+++ shero/src/main/webapp/app/controllers/searchresults.js
@@ -9,61 +9,34 @@
  .controller('SearchResultsCtrl', function($scope, $location, ItemsService) { 
 
 	// Variable for items
-	$scope.itemlist = {};
-    $scope.itemlist = [
-        {id: '1', name:'surfboard',rating:2, price:20, location:'Placa Catalunya 20'},
-        {id: '2', name:'thing',rating:5, price:50, location:'Placa Catalunya 20'},
-        {id: '3', name:'car',rating:5, price:100, location:'Placa Catalunya 20'},
-        {id: '4', name:'skateboard',rating:1, price:10, location:'Placa Catalunya 20'},
-        ];
-    $scope.ratingAndLocation = {rating: 5, location:'Placa Catalunya 5'};
+     $scope.itemlist = {};
+     $scope.itemlist = [
+         {id: '1', name:'surfboard',rating:2, price:20, location:'Placa Catalunya 20'},
+         {id: '2', name:'thing',rating:5, price:50, location:'Placa Catalunya 20'},
+         {id: '3', name:'car',rating:5, price:100, location:'Placa Catalunya 20'},
+         {id: '4', name:'skateboard',rating:1, price:10, location:'Placa Catalunya 20'},
+     ];
+     $scope.ratingAndLocation = {rating: 5, location:'Placa Catalunya 5'};
 	
-	//fetch all items from backend
-	$scope.fetchItems = function() {
-		var getAllItems = ItemsService.getAllItems();
-		getAllItems.then(function(response) {
+     $scope.fetchItems = function() {
+         var getAllItems = ItemsService.getAllItems();
+         getAllItems.then(function(response) {
             //$scope.itemlist = response.data;           
+             $scope.itemlist.forEach(function(item) {
+                 item.availabilityDates = [];
+                 item.availabilityPeriods.forEach(function(availability) {
+                     availabilityDates.push(availability.availabilityDate);
+                 });
+             });
+         });
+     }; 
 
-      $scope.itemlist.forEach(function(item) {
-        item.availabilityDates = [];
-        item.availabilityPeriods.forEach(function(availability) {
-          availabilityDates.push(availability.availabilityDate);
-        });
-      });
-      console.log(JSON.stringify($scope.itemlist));
-    });
-  }; 
 
-    // Call fetch items function when controller is loaded
-    $scope.fetchItems();
-
-    $scope.show = false; 
-
-    $scope.expand = function(){
-      console.log("show")
-      $scope.show = true;
-      $scope.fetchItems();
-      $('#expanding').html('<i class="fa fa-arrow-circle-up fa-5x" style="color:white" ng-click="expand">');
-    };
+     $scope.fetchItems();
      
-        $scope.sort = function(keyname){
-        $scope.sortKey = keyname;   //set the sortKey to the param passed
-        $scope.reverse = !$scope.reverse; //if true make it false and vice versa
-    }
-
-  });
-//to show the rating as stars
-angular.module('SHeroApp').filter('range', function() {
-  return function(input, total) {
-    total = parseInt(total);
-
-    for (var i=0; i<total; i++) {
-      input.push(i);
-    }
-
-    return input;
-  };
-    
- 
+     $scope.sort = function(keyname){
+         $scope.sortKey = keyname;   //set the sortKey to the param passed
+         $scope.reverse = !$scope.reverse; //if true make it false and vice versa
+     }
 
 });
