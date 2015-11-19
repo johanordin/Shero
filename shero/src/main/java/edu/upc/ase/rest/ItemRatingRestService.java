fce0d7ee898cb6ddca21ca9da6240c202e9c2f72
Pascal Jung <pascal.jung@student.hpi.uni-potsdam.de>
2015-11-19 22:00:15 +0100

    extended itemRating: expects rentalId and rating now! updated rental automatically to rated = true once rating is created

--- shero/src/main/java/edu/upc/ase/rest/ItemRatingRestService.java
+++ shero/src/main/java/edu/upc/ase/rest/ItemRatingRestService.java
@@ -16,6 +16,7 @@ import com.googlecode.objectify.ObjectifyService;
 
 import edu.upc.ase.domain.Item;
 import edu.upc.ase.domain.ItemRating;
+import edu.upc.ase.domain.Rental;
 
 @Path("/itemratings")
 public class ItemRatingRestService {
@@ -23,7 +24,7 @@ public class ItemRatingRestService {
 	private static final Gson GSON = new Gson();
 	
 	/**
-	 * Expected format: {"itemId": 141421415215, "userId": 315353155315, "rating":[1..5]}
+	 * Expected format: {"rentalId": 141421415215, "rating":[1..5]}
 	 * @return
 	 */
 	@POST
@@ -31,29 +32,74 @@ public class ItemRatingRestService {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String createItemRating(String jsonItemRating) {
 		JsonObject jsonObj = JSON_PARSER.parse(jsonItemRating).getAsJsonObject();
-		String itemId = jsonObj.get("itemId") != null ? jsonObj.get("itemId").toString().replace("\"", "") : null;
-		String userId = jsonObj.get("userId") != null ? jsonObj.get("userId").toString().replace("\"", "") : null;
+		String rentalId = jsonObj.get("rentalId") != null ? jsonObj.get("rentalId").toString().replace("\"", "") : null;
 		String ratingValue = jsonObj.get("rating") != null ? jsonObj.get("rating").toString().replace("\"", "") : null;
 		
-		ItemRating itemRating;
+		ItemRating newRating = null;
 		
 		// all parameters have to be set
-		if (itemId != null && userId != null && ratingValue != null) {
-			itemRating = new ItemRating(Long.parseLong(itemId), Long.parseLong(userId), Integer.valueOf(ratingValue));
+		if (rentalId != null && ratingValue != null) {
+			// retrieve the rental
+			Rental rental = ObjectifyService.ofy().load().type(Rental.class).filterKey(Key.create(Rental.class, Long.parseLong(rentalId))).first().now();
 			
-			Key<ItemRating> ratingKey = ObjectifyService.ofy().save().entity(itemRating).now();		
-			ItemRating newRating = ObjectifyService.ofy().load().type(ItemRating.class).id(ratingKey.getId()).now();
-			
-			// once rating has been successfully created, update rating counts on respective item
-			Item item = ObjectifyService.ofy().load().type(Item.class).id(Long.parseLong(itemId)).now();
-			item.updateRatingCount(Integer.valueOf(ratingValue));
-			
+			if (!rental.getItemRated()) {
+				// create a new rating for the given user with respect to the given item
+				ItemRating itemRating = new ItemRating(rental.getItemId(), rental.getUserId(), Integer.valueOf(ratingValue));
+				Key<ItemRating> ratingKey = ObjectifyService.ofy().save().entity(itemRating).now();
+				newRating = ObjectifyService.ofy().load().type(ItemRating.class).id(ratingKey.getId()).now();
+				
+				// once rating has been successfully created, update rating counts on respective item
+				Item item = ObjectifyService.ofy().load().type(Item.class).id(rental.getItemId()).now();
+				item.updateRatingCount(Integer.valueOf(ratingValue));
+				// save changes in database
+				ObjectifyService.ofy().save().entity(item).now();
+				
+				// remember in rental, that it has been rated
+				rental.setItemRated();
+				ObjectifyService.ofy().save().entity(rental).now();
+			}
+				
 			return GSON.toJson(newRating);
 		}
 
 		// in other cases return error
 		return "{\"status\":\"error\"}";
 	}
+	
+//	/**
+//	 * Expected format: {"itemId": 141421415215, "userId": 315353155315, "rating":[1..5]}
+//	 * @return
+//	 */
+//	@POST
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String createItemRating(String jsonItemRating) {
+//		JsonObject jsonObj = JSON_PARSER.parse(jsonItemRating).getAsJsonObject();
+//		String itemId = jsonObj.get("itemId") != null ? jsonObj.get("itemId").toString().replace("\"", "") : null;
+//		String userId = jsonObj.get("userId") != null ? jsonObj.get("userId").toString().replace("\"", "") : null;
+//		String ratingValue = jsonObj.get("rating") != null ? jsonObj.get("rating").toString().replace("\"", "") : null;
+//		
+//		ItemRating itemRating;
+//		
+//		// all parameters have to be set
+//		if (itemId != null && userId != null && ratingValue != null) {
+//			itemRating = new ItemRating(Long.parseLong(itemId), Long.parseLong(userId), Integer.valueOf(ratingValue));
+//			
+//			Key<ItemRating> ratingKey = ObjectifyService.ofy().save().entity(itemRating).now();		
+//			ItemRating newRating = ObjectifyService.ofy().load().type(ItemRating.class).id(ratingKey.getId()).now();
+//			
+//			// once rating has been successfully created, update rating counts on respective item
+//			Item item = ObjectifyService.ofy().load().type(Item.class).id(Long.parseLong(itemId)).now();
+//			item.updateRatingCount(Integer.valueOf(ratingValue));
+//			
+//			// remember in rental, that it has been rated
+//			
+//			return GSON.toJson(newRating);
+//		}
+//
+//		// in other cases return error
+//		return "{\"status\":\"error\"}";
+//	}
 	
 	@GET
 	@Path("/{id}")
