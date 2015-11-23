6ba51b46b8be745bc4882a207e46ee0f07a61f00
Max Dyrhage <maxdyrhage@gmail.com>
2015-11-23 16:48:17 +0100

    Added sorting to myRentals

--- shero/src/main/webapp/app/controllers/myRentals.js
+++ shero/src/main/webapp/app/controllers/myRentals.js
@@ -16,6 +16,11 @@ angular.module('SHeroApp')
             });
         };
     
+         $scope.sort = function(keyname){
+             $scope.sortKey = keyname;   //set the sortKey to the param passed
+             $scope.reverse = !$scope.reverse; //if true make it false and vice versa     
+         }
+    
         $scope.rateItem = function (rentalId, rating) {
             var rateData = {};
             rateData.rentalId = rentalId;
