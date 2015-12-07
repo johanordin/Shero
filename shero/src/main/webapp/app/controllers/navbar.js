02d0c1f9ba48e960acf3c515e684b7bc50c1696e
Fabian Boehm <fabian.boehm92@gmail.com>
2015-12-07 17:38:33 +0100

    SearchBar in NavBar not shown on LandingPage

---
+++ shero/src/main/webapp/app/controllers/navbar.js
@@ -0,0 +1,11 @@
+angular.module('SHeroApp')
+    .controller('NavBarCtrl', function ($scope, $state) {
+         $scope.$watch(function () { return $state.current.name; }, function (newVal) {
+            if (newVal === "home") {
+                $scope.home = true;
+            } else {
+                $scope.home = false;
+            }
+             console.log($scope.home);
+        });
+    });
